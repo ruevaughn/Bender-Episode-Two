53bdafc84ff56d3243c7cf19c715a6839be032f8
Chase Jensen <soccerchase@gmail.comm>
2019-12-05 16:24:56 -0700

    Cleanup Classes

--- index.rb
+++ index.rb
@@ -2,45 +2,50 @@
 # the standard input according to the problem statement.
 
 class Bender
+    attr_reader :money_found
+      
     def initialize
-        @bankk_notes = []
+      @money_collected = []
+      @rooms_visited = []
+      @rooms_left_to_visit = []
     end
-    
+      
     def detect_new_bank_notes(room)
-       @bank_note_rooms << room 
+       @rooms_left_to_visit << room 
     end
-    
+      
     def navigate_building
-        @bank_notes_rooms.each do |note|
-            note.money
-        end
+      @rooms_left_to_visit.each do |room|
+       rooms_visited << room 
+      end
     end
-end
-
-class Room
+  end
+  
+  class Room
     attr_reader :number
     attr_reader :money
     attr_reader :first_door
     attr_reader :second_door
+      
     def initialize(attrs)
-        attrs.split!(' ')
-        @number      = attrs[0]
-        @money       = attrs[1]
-        @first_door  = attrs[2]
-        @second_door = attrs[2]
+      attrs.split!(' ')
+      @number      = attrs[0]
+      @money       = attrs[1]
+      @first_door  = attrs[2]
+      @second_door = attrs[2]
     end
-end
-
-
-n = gets.to_i
-bender = Bender.new
-n.times do
-    room_attrs = gets.chomp
-    room = Room.new(room_attrs)
-    bender.detect_new_bank_notes(room)
-end
-
-# Write an action using puts
-# To debug: STDERR.puts "Debug messages..."
-
-puts "answer"
+  end
+  
+  
+  n = gets.to_i
+  bender = Bender.new
+  n.times do
+      room_attrs = gets.chomp
+      room = Room.new(room_attrs)
+      bender.detect_new_bank_notes(room)
+  end
+  
+  # Write an action using puts
+  # To debug: STDERR.puts "Debug messages..."
+  
+  puts "answer"
